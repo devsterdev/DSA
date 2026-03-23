b8ec7432d78fb2aaa1fca19ff4fb01db11b87ddf
devdeepsharma <sharmadevdeep9@gmail.com>
2026-03-23 16:35:55 +0530

    Lower Bound

---
+++ Binary searches/lower_bound.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+int lowerBound(vector<int> &nums, int x){
+  int low = 0;
+  int high = nums.size() - 1;
+  int ans = nums.size();
+  while (low <= high){
+    int mid = (low + high) / 2;
+    if(nums[mid] >= x){
+      ans = mid;
+      high = mid - 1;
+    }
+    else{
+      low = mid + 1;
+    }
+  }
+  return ans;
+}
+
+int main(){
+
+  int n;
+  cout << "Enter the size of sorted arr: ";
+  cin >> n;
+
+  vector<int> arr;
+  cout << "Enter the ele of sorted arr: ";
+  for(int i=0; i<n; i++){
+    int x;
+    cin >> x;
+    arr.push_back(x);
+  }
+
+  int target;
+  cout << "Enter the target: ";
+  cin >> target;
+
+  cout << lowerBound(arr, target);
+
+  return 0;
+}
