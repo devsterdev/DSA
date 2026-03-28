b6d8264a110f3b5e8c7c725a2283b76b60bacb1d
devdeepsharma <sharmadevdeep9@gmail.com>
2026-03-28 18:45:41 +0530

    81. Search in Rotated Sorted Array II

---
+++ Binary searches/search_in_rotated_arr2.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+bool search(vector<int>& nums, int target) {
+  int low = 0;
+  int high = nums.size() - 1;
+  while (low <= high){
+    int mid = (low + high) / 2;
+    if(nums[mid] == target){
+      return true;
+    }
+    if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+      low++;
+      high--;
+    }
+    else if(nums[low] <= nums[mid]){
+      if(nums[low] <= target && target <= nums[mid]){
+        high = mid - 1;
+      }
+      else{
+        low = mid + 1;
+      }
+    }
+    else{
+      if(nums[mid] <= target && target <= nums[high]){
+        low = mid + 1;
+      }
+      else{
+        high = mid - 1;
+      }
+    }
+  }
+  return false;
+}
+
+int main(){
+
+  int n;
+  cout << "Enter the size of sorted arr: ";
+  cin >> n;
+
+  vector<int> arr;
+  cout << "Enter the ele of sorted arr: ";
+  for(int i=0; i<n; i++){
+    int x;
+    cin >> x;
+    arr.push_back(x);
+  }
+
+  int target;
+  cout << "Enter the target: ";
+  cin >> target;
+
+  cout << search(arr, target);
+
+  return 0;
+}
